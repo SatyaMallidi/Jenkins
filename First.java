c5a791a5e91bd0dcdbbf4864580a95e2fd479011
SatyaMallidi <156490218+SatyaMallidi@users.noreply.github.com>
2024-03-10 00:33:22 -0600

    Update First.java

--- First.java
+++ First.java
@@ -1,8 +1,8 @@
 class First{
-  int a=4;
-  int b=5;
-  int c=a+b;
   public static void main(String[] args){
+    int a=4;
+    int b=5;
+    int c=a+b;
     System.out.println("The addition of a and b is :" +c);
   }
 }
